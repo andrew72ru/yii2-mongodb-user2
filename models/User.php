abe04f36e3cdd2f18d3358709a4cf305d635fa9d
Андрей Ждановских <andrew72ru@gmail.com>
2017-02-01 18:57:52 +0500

    Added @todo about Unique validator (cannot validate mongodb model)

--- models/User.php
+++ models/User.php
@@ -108,6 +108,7 @@ class User extends \yii\mongodb\ActiveRecord implements IdentityInterface
 
     /**
      * @inheritdoc
+     * @todo Unique Validator!
      */
     public function rules()
     {
@@ -117,7 +118,7 @@ class User extends \yii\mongodb\ActiveRecord implements IdentityInterface
             ['username', 'match', 'pattern' => static::$usernameRegexp],
             ['email', 'required'],
             ['email', 'email'],
-            [['email', 'username'], 'unique'],
+//            [['email', 'username'], 'unique', 'targetAttribute' => ['email', 'username']],
 //            ['profile_data', 'validateProfile', 'skipOnEmpty' => true],
         ];
     }
